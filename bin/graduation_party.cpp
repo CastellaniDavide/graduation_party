b24662d4656346ad4e4b4fc5084967591a287aa4
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-08-28 16:52:32 +0200

    Created bin/template.cpp

---
+++ bin/graduation_party.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file graduation_party.cpp
+ *
+ * @version 01.01 2020-8-28
+ *
+ * @brief https://training.olinfo.it/#/task/ois_confetti/statement
+ *
+ * @ingroup graduation_party
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+//  uncomment the following lines if you want to read/write from files
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  cin >> N;
+
+  return 0;
+}
